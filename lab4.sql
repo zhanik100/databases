61f0d043bc997b4d8a584398d520aec9848e416f
zhanik100 <43564491+zhanik100@users.noreply.github.com>
2018-10-16 11:47:13 +0600

    Add files via upload

---
+++ lab4.sql
@@ -0,0 +1,34 @@
+CREATE DATABASE lab4;
+CREATE TABLE customers(
+  customer_id INT PRIMARY KEY,
+  cust_name VARCHAR(255),
+  city VARCHAR(255),
+  grade INT,
+  salesman_id INT
+);
+  CREATE TABLE orders(
+    ord_no INT PRIMARY KEY,
+    purch_amt FLOAT,
+    ord_date DATE,
+    customer_id INT REFERENCES customers(customer_id),
+    salesman_id INT REFERENCES customers(salesman_id)
+  );
+  CREATE TABLE salesman(
+    salesman_id INT PRIMARY KEY,
+    name VARCHAR(255),
+    city VARCHAR(255) REFERENCES customers(city),
+    commission FLOAT
+  );
+SELECT sum(purch_amt) FROM orders;
+SELECT avg(purch_amt) FROM orders;
+SELECT count(cust_name) from customers WHERE cust_name NOTNULL;
+SELECT min(purch_amt) FROM orders;
+SELECT * FROM customers WHERE cust_name LIKE '%b';
+SELECT * FROM orders WHERE customer_id IN (SELECT customer_id FROM customers WHERE city='New York');
+SELECT * FROM customers WHERE customer_id IN (SELECT customer_id FROM orders WHERE purch_amt>10);
+SELECT sum(grade) AS "sum_grade" FROM customers;
+SELECT * FROM customers WHERE cust_name NOTNULL;
+SELECT max(grade) FROM customers;
+
+
+
